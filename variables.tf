0732bafa4bb1bd76bea94a4565854f883bae03cf
Theo Bob Massard <tbobm@protonmail.com>
2021-11-05 19:21:55 +0100

    feat(lb): manage load balancers
    - Create resources
    - Provide defaults for load balancer configuration
    - Add tag support
    
    References #1
    
    Signed-off-by: Theo Bob Massard <tbobm@protonmail.com>

--- variables.tf
+++ variables.tf
@@ -1,1 +1,16 @@
-# Define the module's input below
+variable "loadbalancers" {
+  type        = map(any)
+  description = "The Scaleway Load Balancers configurations"
+}
+
+variable "region" {
+  type        = string
+  description = "Region to be used with the Scaleway provider"
+  default     = "fr-par"
+}
+
+variable "zone" {
+  type        = string
+  description = "Zone to be used with the Scaleway provider"
+  default     = "fr-par-1"
+}
--- versions.tf
+++ versions.tf
@@ -1,1 +1,9 @@
-# Specify required provider versions below
+terraform {
+  required_providers {
+    scaleway = {
+      source  = "scaleway/scaleway"
+      version = "~> 2.0"
+    }
+  }
+  required_version = ">= 0.13"
+}
